6243c0973ba805de91760a9df3c42b3af39150fe
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2020-02-05 15:49:33 +0100

    Merge #17812: extract conditional to bool CNetAddr::IsHeNet
    https://github.com/bitcoin/bitcoin/pull/17812/commits/c90b9a2399f4cead37bad39f388ce1255e123dc4

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -200,6 +200,11 @@ bool CNetAddr::IsRFC7343() const
            GetByte(13) == 0x00 && (GetByte(12) & 0xF0) == 0x20;
 }
 
+bool CNetAddr::IsHeNet() const
+{
+    return (GetByte(15) == 0x20 && GetByte(14) == 0x01 && GetByte(13) == 0x04 && GetByte(12) == 0x70);
+}
+
 bool CNetAddr::IsTor() const { return m_net == NET_ONION; }
 
 bool CNetAddr::IsLocal() const
@@ -440,12 +445,15 @@ std::vector<unsigned char> CNetAddr::GetGroup(const std::vector<bool> &asmap) co
         nStartByte = 6;
         nBits = 4;
     }
-    // for he.net, use /36 groups
-    else if (GetByte(15) == 0x20 && GetByte(14) == 0x01 && GetByte(13) == 0x04 && GetByte(12) == 0x70)
+    else if (IsHeNet())
+    {
+        // for he.net, use /36 groups
         nBits = 36;
-    // for the rest of the IPv6 network, use /32 groups
-    else
+    }
+    else {
+        // for the rest of the IPv6 network, use /32 groups
         nBits = 32;
+    }
 
     while (nBits >= 8)
     {
