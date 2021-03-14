8bc60430e089821e5361fe4103fda392981c0cb2
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2021-03-14 13:17:01 +0530

    Merge #16702: Return mapped AS in RPC call getpeerinfo
    https://github.com/bitcoin/bitcoin/pull/16702/commits/e4658aa8eaf1629dd5af8cf7b9717a8e72028251

--- src/net.h
+++ src/net.h
@@ -774,6 +774,7 @@ public:
     CAddress addr;
     // Bind address of our side of the connection
     CAddress addrBind;
+    uint32_t m_mapped_as;
     // In case this is a verified MN, this value is the proTx of the MN
     uint256 verifiedProRegTxHash;
     // In case this is a verified MN, this value is the hashed operator pubkey of the MN
@@ -1114,7 +1115,7 @@ public:
 
     void CloseSocketDisconnect(CConnman* connman);
 
-    void copyStats(CNodeStats &stats);
+    void copyStats(CNodeStats &stats, std::vector<bool> &m_asmap);
 
     ServiceFlags GetLocalServices() const
     {
